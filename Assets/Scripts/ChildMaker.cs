e96b237915c48e382f7ee47400bfcdfaa6565ccb
Caina <cainacc@hotmail.com>
2020-04-05 17:53:37 -0300

    first version of code to move cards instead of teleport them

--- Assets/Scripts/ChildMaker.cs
+++ Assets/Scripts/ChildMaker.cs
@@ -2,19 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChildMaker
+public class ChildMaker : MonoBehaviour
 {
-    public static void AdoptAndTeleport(Transform father, RectTransform child)
+    public static void AdoptAndTeleport(Transform parent, RectTransform child)
     {
-        child.position = father.position;
-        child.GetComponent<RectTransform>().SetParent(father, true);
+        child.position = parent.position;
+        child.GetComponent<RectTransform>().SetParent(parent, true);
     }
 
-    public static void AdoptTeleportAndScale(Transform father, RectTransform child)
+    public static void AdoptTeleportAndScale(Transform parent, RectTransform child)
     {
-        child.position = father.position;
+        child.position = parent.position;
         RectTransform childTransform = child.GetComponent<RectTransform>();
-        childTransform.SetParent(father, true);
+        childTransform.SetParent(parent, true);
         childTransform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+    }
+
+    public static void AdoptAndSmoothlyMoveToParent(Transform parent, RectTransform child, float totalTime)
+    {
+        GameObject childMaker = new GameObject();
+        childMaker.AddComponent(typeof(ChildMaker));
+        ChildMaker maker = childMaker.GetComponent<ChildMaker>();
+
+        child.GetComponent<RectTransform>().SetParent(parent, true);
+
+        maker.SmoothlyMoveChildToParentPosition(parent, child, totalTime);
+    }
+
+    private void SmoothlyMoveChildToParentPosition(Transform parent, RectTransform child, float totalTime)
+    {
+        StartCoroutine(SmothlyMoveToPos(parent, child, totalTime));
+    }
+
+    IEnumerator SmothlyMoveToPos(Transform parent, RectTransform child, float totalTime)
+    {
+        Vector3 initialPosition = child.transform.position;
+
+        Vector3 direction = parent.transform.position - child.transform.position;
+
+        float time = 0.0f;
+
+        while (time < totalTime)
+        {
+            time += Time.deltaTime;
+
+            float completePercentage = time / totalTime;
+
+            child.transform.position = initialPosition + completePercentage * direction;
+
+            yield return null;
+        }
+
+        child.transform.position = parent.transform.position;
+
+        Destroy(gameObject);
     }
 }   
