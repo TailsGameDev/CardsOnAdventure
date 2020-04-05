41f745dc5649aa04a92948800d3a1699feebd29e
Caina <cainacc@hotmail.com>
2020-04-05 20:15:47 -0300

    steal block when steal is blocked

--- Assets/Scripts/Skills/Special Effects/StealEffect.cs
+++ Assets/Scripts/Skills/Special Effects/StealEffect.cs
@@ -7,7 +7,15 @@ public class StealEffect : SpecialEffect
     public override void ExecuteEffect(Battlefield obf, Battlefield abf, ref int targetIndex, GameObject specialVFX)
     {
         Card attacker = abf.GetSelectedCard();
-        Card target = obf.GetReferenceToCardAt(targetIndex);
+        Card target;
+        if (BlockEffect.IsThereABlockerInTheFrontOfTarget(obf, targetIndex))
+        {
+            target = obf.GetCardInFrontOf(targetIndex);
+        } 
+        else
+        {
+            target = obf.GetReferenceToCardAt(targetIndex);
+        }
         attacker.Skills = target.Skills;
         target.Skills = skillsMediator.GetComponent<TheOnlySkillsMediatorInstance>().GetBasicAttackSkill();
     }
